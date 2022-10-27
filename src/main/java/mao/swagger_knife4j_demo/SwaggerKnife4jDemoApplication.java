825fdda90f03ef78faf2f7ca97c4ae2cf863dbee
mao <1296193245@qq.com>
2022-10-27 13:14:01 +0800

    swagger_knife4j_demo

---
+++ src/main/java/mao/swagger_knife4j_demo/SwaggerKnife4jDemoApplication.java
@@ -0,0 +1,15 @@
+package mao.swagger_knife4j_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerKnife4jDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SwaggerKnife4jDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/swagger_knife4j_demo/SwaggerKnife4jDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.swagger_knife4j_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerKnife4jDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
